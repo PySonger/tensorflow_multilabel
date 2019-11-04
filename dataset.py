19f651a3d698dcf81238482ffba806c3a80573e7
song <sding@nullmax.ai>
2019-11-04 20:22:09 +0800

    Fix one bug in dataset.py

--- dataset.py
+++ dataset.py
@@ -2,14 +2,16 @@
 @Author: Ding Song
 @Date: 2019-10-31 00:50:16
 @LastEditors: Ding Song
-@LastEditTime: 2019-10-31 01:44:56
+@LastEditTime: 2019-11-04 17:32:53
 @Description: A dataset compliment with TensorFlow.
 '''
 import os
+import copy
 import cv2
 import tensorflow as tf 
 import numpy as np
-
+import random as rd
+import matplotlib.pyplot as plt
 
 class MakeDataFile(object):
 
@@ -34,9 +36,10 @@ class MakeDataFile(object):
 
 class Dataset(object):
 
-    def __init__(self,data_file,img_size,batch_size,epoch_num):
+    def __init__(self,data_file,img_size,crop_size,batch_size,epoch_num):
         self.data_file = data_file
         self.img_size = img_size
+        self.crop_size = crop_size
         self.batch_size = batch_size
         self.epoch_num = epoch_num
 
@@ -56,8 +59,8 @@ class Dataset(object):
         file_paths = tf.constant(path_list)
         labels = tf.constant(label_list)
         dataset = tf.data.Dataset.from_tensor_slices((file_paths,labels))
-        dataset.map(self.parser_function)
-        dataset.shuffle(buffer_size=1000).batch(self.batch_size).repeat(self.epoch_num)
+        dataset = dataset.map(self.parser_function)
+        dataset = dataset.shuffle(buffer_size=1000).batch(self.batch_size).repeat(self.epoch_num)
         iterator = dataset.make_one_shot_iterator()
         return iterator
 
@@ -65,26 +68,39 @@ class Dataset(object):
         image_string = tf.read_file(file_path)
         image_decoded = tf.image.decode_jpeg(image_string)
         image_decoded = tf.image.resize_images(image_decoded,self.img_size)
-        return image_decoded,label
+
+        image_decoded = tf.reshape(image_decoded,[1,tf.shape(image_decoded)[0],tf.shape(image_decoded)[1],3])
+        box_ind = tf.constant([0],dtype=tf.int32)
+        crop_rate = [float(self.crop_size[0])/self.img_size[0], float(self.crop_size[1])/self.img_size[1]]
+        y,x = rd.uniform(0,1 - crop_rate[0]),rd.uniform(0,1 - crop_rate[1])
+        boxes = tf.constant([[y,x,crop_rate[0],crop_rate[1]]],dtype=tf.float32)
+        crop_size = tf.constant(self.crop_size,dtype=tf.int32)
+
+        crop_boxes = tf.image.crop_and_resize(image_decoded,boxes,box_ind=box_ind,crop_size=crop_size)
+        return crop_boxes[0],label
         
 def main():
-    img_dir = '/media/song/机械盘1/my_python_file/dataset/天气识别/Train'
-    save_dir = '/media/song/机械盘1/my_python_file/dataset/天气识别/'
-    make_data_file = MakeDataFile(img_dir,save_dir)
-    make_data_file.make_file()
-    data_file = os.path.join(save_dir,'data_file.txt')
-    data = Dataset(data_file,[50,50],2,1)
+    #img_dir = '/media/song/机械盘1/my_python_file/dataset/天气识别/Train'
+    #save_dir = '/media/song/机械盘1/my_python_file/dataset/天气识别/'
+    #make_data_file = MakeDataFile(img_dir,save_dir)
+    #make_data_file.make_file()
+    #data_file = os.path.join(save_dir,'data_file.txt')
+    data_file = '/media/song/Bigger_Disk/tianjin_data/traffic-light-tianjin-rcb/new_data_file.txt'
+    data = Dataset(data_file,[50,50],[48,48],1,5)
     iterator = data.make_data()
     one_element =  iterator.get_next()
     with tf.Session() as sess:
         i = 0
         try:
-            while True:
+            while i < 10:
                 print(i)
-                print(sess.run(one_element))
+                img,label = sess.run(one_element)
+                print(img.shape)
+                print(label)
                 i += 1
         except:
             print('done...')
+
 
 if __name__ == "__main__":
     main()
--- solver.py
+++ solver.py
@@ -2,7 +2,7 @@
 @Author: Ding Song
 @Date: 2019-10-31 20:03:45
 @LastEditors: Ding Song
-@LastEditTime: 2019-11-01 19:05:11
+@LastEditTime: 2019-11-04 20:19:16
 @Description: train, evalution and test part.
 '''
 import os
@@ -10,14 +10,23 @@ import tensorflow as tf
 from dataset import Dataset
 from model import MultiLabelLenet
 
+os.environ['CUDA_VISIABLE_DEVICES'] = '0'
+
 class Solver(object):
 
-    def __init__(self,device_id,save_dir,num_classes):
-        self.device_id = device_id
+    def __init__(self,device_id,save_dir,num_classes,train_data_file,img_size,crop_size,batch_size,epoch_num,learning_rate):
         self.save_dir = save_dir
-        self.model = MultiLabelLenet(num_classes)
-        pass
+        self.device_id = device_id
+        self.train_data = Dataset(train_data_file,img_size,crop_size,batch_size,epoch_num)
+        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+        self.loss = 
+        config = tf.ConfigProto()
+        config.gpu_options.all_growth = True
+        self.sess = tf.Session(config=config)
 
-    def train(self):
-        with tf.device('/gpu:'+str(self.device_id)):
+        with tf.name_scope('build net'):
+            s
+
+    def train(self,train_data):
+
             
